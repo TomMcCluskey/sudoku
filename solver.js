c76b25bbda11aa6b06b128a35b3a2c6afab8b6ab
Tom McCluskey <bosun.tom@gmail.com>
2015-06-18 16:01:17 -0700

    Initial Commit

---
+++ solver.js
@@ -0,0 +1,24 @@
+module.exports = Solver;
+
+var Game = function(initialString) {
+
+  this.cells = [];
+  this.rows = [];
+  this.cols = [];
+  this.blocks = [];
+
+  for (i = 0; i < initialString.length; i++) {
+    cells.push(new Cell(initialString[i]));
+  }
+
+
+
+  var Cell = function(value) {
+    this.value = value;
+  };
+
+  var Group = function(cells) {
+
+  };
+
+};
