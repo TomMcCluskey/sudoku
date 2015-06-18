4ac0e00418fd1301f7e5c6998e11f89b2a29e0ea
Tom McCluskey <bosun.tom@gmail.com>
2015-06-18 16:50:14 -0700

    parsing of string into group and cell objects finished

--- solver.js
+++ solver.js
@@ -1,24 +1,42 @@
 module.exports = Solver;
 
+var trial = '158.2..6.2...8..9..3..7.8.2.6.74......4.6.7......19.5.4.9.3..2..2..5...8.7..9.413';
+
 var Game = function(initialString) {
 
   this.cells = [];
   this.rows = [];
   this.cols = [];
   this.blocks = [];
+  this.rowSize = Math.floor(Math.sqrt(initialString.length));
+
+  var Cell = function(value, row, col, block) {
+    this.value = value;
+    this.row = row;
+    this.col = col;
+    this.block = block;
+  };
 
   for (i = 0; i < initialString.length; i++) {
-    cells.push(new Cell(initialString[i]));
+    var rowNum = Math.floor(i/this.rowSize);
+    var colNum = i % this.rowSize;
+    var blockNum = Math.floor(colNum / 3) + 3 * (Math.floor(rowNum / 3));
+    this.cells.push(new Cell(initialString[i], rowNum, colNum, blockNum));
+    this.rows[rowNum] = this.cells.filter( function(cell) {
+      return cell.row === rowNum;
+    });
+    this.cols[colNum] = this.cells.filter( function(cell) {
+      return cell.col === colNum;
+    });
+    this.blocks[blockNum] = this.cells.filter( function(cell) {
+      return cell.block === blockNum;
+    });
   }
 
 
 
-  var Cell = function(value) {
-    this.value = value;
-  };
-
   var Group = function(cells) {
-
+    this.members = cells;
   };
 
 };
