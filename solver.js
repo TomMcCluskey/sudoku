5a293cdabebbec9e8d2c3a212170a485fd01dc8e
Tom McCluskey <bosun.tom@gmail.com>
2015-06-18 17:15:37 -0700

    ok, actually have group objects being created this time

--- solver.js
+++ solver.js
@@ -1,4 +1,4 @@
-module.exports = Solver;
+module.exports = Game;
 
 var trial = '158.2..6.2...8..9..3..7.8.2.6.74......4.6.7......19.5.4.9.3..2..2..5...8.7..9.413';
 
@@ -8,7 +8,7 @@ var Game = function(initialString) {
   this.rows = [];
   this.cols = [];
   this.blocks = [];
-  this.rowSize = Math.floor(Math.sqrt(initialString.length));
+  this.rowSize = 9;
 
   var Cell = function(value, row, col, block) {
     this.value = value;
@@ -17,26 +17,44 @@ var Game = function(initialString) {
     this.block = block;
   };
 
+  var Group = function(cells) {
+    this.members = cells;
+  };
+
+  var _rows = [],
+      _cols = [],
+      _blocks = [];
+
   for (i = 0; i < initialString.length; i++) {
     var rowNum = Math.floor(i/this.rowSize);
     var colNum = i % this.rowSize;
     var blockNum = Math.floor(colNum / 3) + 3 * (Math.floor(rowNum / 3));
     this.cells.push(new Cell(initialString[i], rowNum, colNum, blockNum));
-    this.rows[rowNum] = this.cells.filter( function(cell) {
+    _rows[rowNum] = this.cells.filter( function(cell) {
       return cell.row === rowNum;
     });
-    this.cols[colNum] = this.cells.filter( function(cell) {
+    _cols[colNum] = this.cells.filter( function(cell) {
       return cell.col === colNum;
     });
-    this.blocks[blockNum] = this.cells.filter( function(cell) {
+    _blocks[blockNum] = this.cells.filter( function(cell) {
       return cell.block === blockNum;
     });
   }
 
+  _rows.forEach(function(row) {
+    this.rows.push(new Group(row));
+  }, this);
 
+  _cols.forEach(function(col) {
+    this.cols.push(new Group(col));
+  }, this);
 
-  var Group = function(cells) {
-    this.members = cells;
-  };
+  _blocks.forEach(function(block) {
+    this.blocks.push(new Group(block));
+  }, this);
+
+console.log(this);
+
 
 };
+var game = new Game(trial);
