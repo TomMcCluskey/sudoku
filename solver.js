ca3a5315f0a735bd3e2df06d1fdb4ba9bbebb0e5
Tom McCluskey <bosun.tom@gmail.com>
2015-06-19 13:44:55 -0700

    Solver in progress

--- solver.js
+++ solver.js
@@ -1,6 +1,5 @@
 module.exports = Game;
 
-
 var trial = '158.2..6.2...8..9..3..7.8.2.6.74......4.6.7......19.5.4.9.3..2..2..5...8.7..9.413';
 
 var Game = function(initialString) {
@@ -25,17 +24,28 @@ var Game = function(initialString) {
     return _.intersection(this.row.missingValues(), this.col.missingValues(), this.block.missingValues());
   };
 
+  Cell.prototype.siblings = function() {
+    var rowSiblings = this.row.members.filter(function(cell) {
+      return cell.colNum !== this.colNum;
+    }, this);
+    var colSiblings = this.col.members.filter(function(cell) {
+      return cell.rowNum !== this.rowNum;
+    }, this);
+    var blockSiblings = this.block.members.filter(function(cell) {
+      return (cell.rowNum !== this.rowNum) && (cell.colNum !== this.colNum);
+    }, this);
+    return _.union(rowSiblings, colSiblings, blockSiblings);
+  };
+
   var Group = function(cells, type) {
     this.members = cells;
     this.values = this.members.map(function(item) { return item.value; } );
-    //console.log(cells);
     var group = this;
     this.emptyCells = this.members.filter(function(cell) {
       return cell.value === '.';
     });
     this.members.forEach(function(member) {
       member[type] = group;
-      //console.log(member);
     });
   };
 
@@ -82,7 +92,11 @@ var Game = function(initialString) {
     this.blocks.push(new Group(block, 'block'));
   }, this);
 
-// console.log(this);
+  this.unsolved = function() {
+    return this.cells.filter(function(cell) {
+      return cell.value === '.';
+    });
+  };
 
 
 };
@@ -91,6 +105,24 @@ Game.prototype.solve = function() {
   // look in each group for unsolved squares.
   // check each unsolved square for possibilities
   // check each other square in group for that possibility
+  this.unsolved().forEach(function(cell) {
+    console.log(cell);
+    if (cell.possibileValues().length === 1) {
+      cell.value = cell.possibleValues()[0];
+    } else {
+      //if () {} // need a cell.siblings property
+      cell.possibleValues().forEach(function(value) {
+        var elsewhere = false;
+        cell.siblings().forEach(function(sibling) {
+          if (sibling.value === value) {
+            elsewhere = true;
+            return;
+          }
+        });         
+        if(!elsewhere) { cell.value = value; }
+      });
+    }
+  });
 };
 
 Game.prototype.print = function() {
@@ -103,5 +135,10 @@ var game = new Game(trial);
 // console.log(game.rows[0].missingValues());
 // console.log(game.cells[11]);
 // console.log(game.cells[11].row.missingValues());
+// console.log(game.unsolved())
 // console.log(game.cells[11].possibleValues());
 // console.log(game.rows[3].emptyCells);
+// game.cells.forEach(function(cell) {console.log('row: ' + cell.rowNum + '; col: ' + cell.colNum + '; block: ' + cell.blockNum);});
+// game.cells[11].siblings().forEach(function(cell) { console.log('row: ' + cell.rowNum + '; col: ' + cell.colNum + '; block: ' + cell.blockNum); });
+game.solve();
+// game.cells.forEach(function(cell) {console.log(cell.value);});
